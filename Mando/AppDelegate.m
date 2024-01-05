8f735016fe0eda064ff25b44ea0937c8043d1446
Mario Diana <software@mariodiana.com>
2024-01-05 15:35:10 -0500

    Modify formatting of file

--- Mando/AppDelegate.m
+++ Mando/AppDelegate.m
@@ -8,11 +8,9 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
